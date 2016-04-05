0a1b295f0002e3344cde6b6d2e65477d006d542c
William Seviour <william.seviour@gmail.com>
2016-04-05 10:53:45 -0400

    Added markdown references

--- .emacs.d/settings/markdown-settings.el
+++ .emacs.d/settings/markdown-settings.el
@@ -2,6 +2,8 @@
 ;;; Markdown mode ;;;
 ;-------------------;
 
+(require 'reftex)
+
 (include-plugin "markdown-mode")
 (autoload 'markdown-mode "markdown-mode.el"
   "Major mode for editing Markdown files" t)
@@ -17,6 +19,14 @@
 	  (lambda ()
 	    (turn-on-auto-fill)
             (fci-mode 1)))
+
+;;; Code:
+(defvar reftex-cite-format-markdown
+  '((?\C-m . "[@%l]")
+    (?k . "@%l")
+    )
+        "Reftex citation format compatible with pandoc markdown.")
+
 
 
 (provide 'markdown-settings)
